b5195abdb31624265dc5e4cc335bd2f6ec881236
Your Name <your.email@example.com>
2024-11-08 00:10:28 +0400

    mesame davaleba bolomde

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,62 @@
 public class Main {
     public static void main(String[] args) {
 
-        String String ="/";
-        double x = 1;
-        double y = 0;
+        // mtliani bolo davaleba ert kodshi chavsvi:
+        // average aris sashualos gageba,
+        //parity aris kent-luwobis gageba,
+        // comparing aris shedareba
+        //addition aris damateba 4nishniaze metebis,
+        //triangle aris samkutxeti tu sheidzleba gaxdes magis gageba
+        String task ="triangle";
 
-        if(String == "+") {
-            System.out.println(x + y);
+
+        double x = 90;
+        double y = 45;
+        double z = 45;
+        double k = 5413;
+
+        double answer = 0;
+
+        if (task == "average"){
+            answer = (x+y+z)/3;
+            System.out.println(answer);
         }
-        if(String == "-") {
-            System.out.println(x - y);
+
+        if (task == "parity") {
+            if (x%2 >0) {
+                System.out.println("kenti");
+            } else {
+                System.out.println("kenti");
+            }
         }
-        if(String == "*") {
-            System.out.println(x * y);
+
+        if (task == "comparing") {
+            if (x > y) {
+                System.out.println("x aris meti");
+            } else{
+                if (x < y) {
+                    System.out.println("y aris meti");
+                } else {
+                    System.out.println("tolia");
+                }
+            }}
+
+            if (task == "addition") {
+                if (x<1000){x=0;}
+                if (y<1000){y=0;}
+                if (z<1000){z=0;}
+                if (k<1000){k=0;}
+
+                System.out.println(x+y+z+k);
+            }
+
+
+            if (task == "triangle") {
+                if ((x+y+z) == 180) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            }
         }
-        if(String == "/") {
-            System.out.println(x / y);
         }
-        }
-    }
