9bd4aa338bf57163f2caf6cf3a2df42d9cb67278
Your Name <your.email@example.com>
2024-11-07 21:52:57 +0400

    pirveli davalebis pirveli nomeri

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double First = 10.234;
+
+        System.out.println("10.234 aramteli nawili: " + First % 1);
+
+        // es adgens ra nawili aris aramteli
+    }
+}
