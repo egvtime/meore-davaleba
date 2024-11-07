f8e7156d7e9df10d16e1b246c2cebe44bde101ec
Your Name <your.email@example.com>
2024-11-07 23:27:13 +0400

    meore davalebis pirveli ori davaleba (temperaturis kalkulatori)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        int time = 3762; //second
+        // 0 gaxadet is romelic gindat gamotvalot
 
-            int hour;
-            int minute;
-            int second;
+        double F = 1;
+        double C = 0;
 
-            hour = time/3600;
-            minute = (time%3600)/60;
-            second = (time%60);
+        if (C==0 & F ==0){
+            System.out.println("0.0C=32F and 0.0F=-17.(7)");
+        }
+        if (C!=0 & F!=0) {
+            System.out.println("this equation is disabled");
+        }
 
-        System.out.println(hour + ":" + minute + ":" + second );
+        if (F == 0 & C != 0){
+            F=(C*1.8)+32;
+            System.out.println(C + "C=" + F);
+        }
+        if (C == 0 & F != 0){
+            C=(F-32)/1.8;
+            System.out.println(F + "F=" + C);
+        }
     }}
